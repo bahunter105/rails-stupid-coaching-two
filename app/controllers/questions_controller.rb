8fc25a101fe77d0a31f71ecd1f81bf750f66703f
Hunter Bourgeois <bahunter@gmail.com>
2022-01-16 14:10:49 +0100

    logic works, but not so pretty

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,19 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params[:question]
+
+   if @question == "I am going to work"
+    @coach_response = "Great!"
+   elsif @question[-1] == "?"
+    @coach_response = "Silly question, get dressed and go to work!"
+   else
+    @coach_response = "I don't care, get dressed and go to work!"
+   end
+
+    # If the message is I am going to work, the coach will answer Great!
+    # If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
+    # Otherwise the coach will answer I don't care, get dressed and go to work!
 
   end
 end
